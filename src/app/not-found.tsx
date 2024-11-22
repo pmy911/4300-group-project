10e35fb813aadd1f67906391f47e15203576850f
dman4703 <doquinn32@gmail.com>
2024-11-22 00:23:49 -0500

    updated not-found ui design

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,25 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import React from 'react';
+import Logo from "@/app/components/Logo";
 
 export default function NotFound() {
     return (
-        <div>
-            <h2>Not Found</h2>
-            <p>Could not find requested resource</p>
-            <Link href="/">Return Home</Link>
+        <div className="flex flex-col space-y-2.5 min-h-screen">
+            {/* Header section with the logo */}
+            <div className="flex flex-row p-1 justify-center items-center">
+                <Link href="/">
+                    <Logo />
+                </Link>
+            </div>
+            <hr style={{ height: '2px', backgroundColor: '#A8A8A7', border: 'none' }} />
+            {/* Main content area for the error message */}
+            <div className="flex flex-col flex-grow justify-center p-10 space-y-5">
+                <p className="text-[125px] leading-tight">not found:</p>
+                <p className="text-[125px] leading-tight">could not find requested resource.</p>
+                <Link href="/">
+                    <p className="text-[125px] leading-tight underline">› Return Home ‹</p>
+                </Link>
+            </div>
         </div>
-    )
+    );
 }
